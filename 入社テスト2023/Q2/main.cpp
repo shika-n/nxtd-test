c2eb6b0a162fca43b983c4d0fc7fa227828b00a0
Shika <greffinroid@ymail.com>
2024-03-02 01:19:47 +0900

    Cleaned code following the Java version

--- 入社テスト2023/Q2/main.cpp
+++ 入社テスト2023/Q2/main.cpp
@@ -11,6 +11,56 @@ struct Rule {
 	int gain_after_sleep; // S - Gain speed by S after sleep for 3 hours
 };
 
+class Job {
+	private:
+		int hours_spent;
+		int current_speed;
+		int lines_to_write_left;
+
+	public:
+		Job(int hours_spent, int current_speed, int lines_to_write_left) : 
+			hours_spent(hours_spent),
+			current_speed(current_speed),
+			lines_to_write_left(lines_to_write_left)
+		{}
+
+		void develop(const Rule &rule) {
+			lines_to_write_left -= current_speed;
+			current_speed = std::max(0, current_speed - rule.reduce_after_dev);
+			hours_spent += 1;
+		}
+
+		void sleep(const Rule &rule) {
+			hours_spent += 3;
+			current_speed = std::min(current_speed + rule.gain_after_sleep, rule.max_speed);
+		}
+		
+		static std::pair<int, int> get_potential_and_hours(const Rule &rule, int speed) {
+			int pot = 0;
+			int hours_taken = 0;
+
+			while (speed > 0) {
+				pot += speed;
+				speed -= rule.reduce_after_dev;
+				hours_taken += 1;
+			}
+
+			return std::pair(pot, hours_taken);
+		}
+
+		int get_hours_spent() {
+			return hours_spent;
+		}
+
+		int get_current_speed() {
+			return current_speed;
+		}
+
+		int get_lines_to_write_left() {
+			return lines_to_write_left;
+		}
+};
+
 Rule get_input() {
 	Rule rule { 0, 0, 0, 0 };
 	std::cin >> rule.target >> rule.max_speed >> rule.reduce_after_dev >> rule.gain_after_sleep;
@@ -46,44 +96,34 @@ int get_potential(Rule rule, int speed, int *hours_taken_ptr = nullptr) {
 	return pot;
 }
 
-void sleep(Rule rule, int *hours_spent, int *current_speed) {
-	*hours_spent += 3;
-	*current_speed = std::min(*current_speed + rule.gain_after_sleep, rule.max_speed);
-	// std::cout << "SLEEP" << std::endl;
-	// std::cout << "SLP, Time: " << hours_spent << ", Written: " << lines_of_codes_written << "/" << rule.target << ", Current speed: " << current_speed << std::endl;
-}
-
 int main() {
 	Rule rule = get_input();
 	// Rule rule =	get_input_from_file("tc.txt");
-	int hours_spent = 0;
-	int current_speed = rule.max_speed;
-	int lines_to_write_left = rule.target;
+	Job job(0, rule.max_speed, rule.target);
 
-	while (lines_to_write_left > 0) {
+	while (job.get_lines_to_write_left() > 0) {
 		// std::cout << "BEF, Time: " << hours_spent << ", Left: " << lines_to_write_left << "/" << rule.target << ", Current speed: " << current_speed << std::endl;
-		if (current_speed <= 0) { // speedが0になると寝るしかない
-			sleep(rule, &hours_spent, &current_speed);
+		if (job.get_current_speed() <= 0) { // speedが0になると寝るしかない
+			job.sleep(rule);
 		}
 
-		int hours_taken = 0;
-		int current_potential = get_potential(rule, current_speed, &hours_taken);
-		float current_efficiency = 1.0f * current_potential / hours_taken;
+		std::pair<int, int> current_potential_hours = Job::get_potential_and_hours(rule, job.get_current_speed());
+		int current_potential = current_potential_hours.first;
+		float current_efficiency = 1.0f * current_potential / current_potential_hours.second;
 
-		int potential_after_sleep = get_potential(rule, std::min(current_speed + rule.gain_after_sleep, rule.max_speed), &hours_taken);
-		float sleep_efficiency = 1.0f * potential_after_sleep / (hours_taken + 3);
+		std::pair<int, int> sleep_potential_hours = Job::get_potential_and_hours(rule, std::min(job.get_current_speed() + rule.gain_after_sleep, rule.max_speed));
+		int potential_after_sleep = sleep_potential_hours.first;
+		float sleep_efficiency = 1.0f * potential_after_sleep / (sleep_potential_hours.second + 3);
 
 		// std::cout << "EFF: " << current_efficiency << " vs " << sleep_efficiency << std::endl;
 
 		// 寝るか開発かどっちが有利
-		if (current_potential < lines_to_write_left && sleep_efficiency >= current_efficiency) {
+		if (current_potential < job.get_lines_to_write_left() && sleep_efficiency >= current_efficiency) {
 			// 寝る
-			sleep(rule, &hours_spent, &current_speed);
+			job.sleep(rule);
 		} else {
 			// 開発
-			lines_to_write_left -= current_speed;
-			current_speed = std::max(0, current_speed - rule.reduce_after_dev);
-			hours_spent += 1;
+			job.develop(rule);
 			// std::cout << "DEV" << std::endl;
 		}
 
@@ -91,5 +131,5 @@ int main() {
 		// std::cout << "--\n";
 	}
 
-	std::cout << hours_spent << std::endl;
+	std::cout << job.get_hours_spent() << std::endl;
 }
