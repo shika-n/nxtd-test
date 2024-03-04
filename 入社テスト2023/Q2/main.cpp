87d6f9849334a426ad3038d52263bd6cbfdef5db
Shika <greffinroid@ymail.com>
2024-03-04 16:53:16 +0900

    Updated answer for 1-2

--- 入社テスト2023/Q2/main.cpp
+++ 入社テスト2023/Q2/main.cpp
@@ -11,54 +11,10 @@ struct Rule {
 	int gain_after_sleep; // S - Gain speed by S after sleep for 3 hours
 };
 
-class Job {
-	private:
-		int hours_spent;
-		int current_speed;
-		int lines_to_write_left;
-
-	public:
-		Job(int hours_spent, int current_speed, int lines_to_write_left) : 
-			hours_spent(hours_spent),
-			current_speed(current_speed),
-			lines_to_write_left(lines_to_write_left)
-		{}
-
-		void develop(const Rule &rule) {
-			lines_to_write_left -= current_speed;
-			current_speed = std::max(0, current_speed - rule.reduce_after_dev);
-			hours_spent += 1;
-		}
-
-		void sleep(const Rule &rule) {
-			hours_spent += 3;
-			current_speed = std::min(current_speed + rule.gain_after_sleep, rule.max_speed);
-		}
-		
-		static std::pair<int, int> get_potential_and_hours(const Rule &rule, int speed) {
-			int pot = 0;
-			int hours_taken = 0;
-
-			while (speed > 0) {
-				pot += speed;
-				speed -= rule.reduce_after_dev;
-				hours_taken += 1;
-			}
-
-			return std::pair(pot, hours_taken);
-		}
-
-		int get_hours_spent() {
-			return hours_spent;
-		}
-
-		int get_current_speed() {
-			return current_speed;
-		}
-
-		int get_lines_to_write_left() {
-			return lines_to_write_left;
-		}
+enum class State {
+	kRoot,
+	kSleep,
+	kDevelop
 };
 
 Rule get_input() {
@@ -71,65 +27,42 @@ Rule get_input_from_file(const char *file_path) {
 	std::ifstream fs(file_path);
 	
 	if (fs.is_open()) {
-		char ch[10];
 		std::cin.rdbuf(fs.rdbuf());
 		return get_input();
 	}
 	return Rule { 0, 0, 0, 0 };
 }
 
-// speedが0まで、どれくらい行数がかける。そのかかった時間もどのくらいかかる。
-int get_potential(Rule rule, int speed, int *hours_taken_ptr = nullptr) {
-	int pot = 0;
-	int hours_taken = 0;
-
-	while (speed > 0) {
-		pot += speed;
-		speed -= rule.reduce_after_dev;
-		hours_taken += 1;
+int get_hours(const Rule &rule, int hours_spent, int current_speed, int lines_left_to_write, State state) {
+	if (current_speed <= 0) { // Force Sleep
+		hours_spent += 3;
+		current_speed = rule.gain_after_sleep;
+	} else if (current_speed >= rule.max_speed) { // Force Develop
+		hours_spent += 1;
+		lines_left_to_write -= current_speed;
+		current_speed = std::max(current_speed - rule.reduce_after_dev, 0);
+	} else if (state == State::kSleep) { // Sleep
+		hours_spent += 3;
+		current_speed = std::min(current_speed + rule.gain_after_sleep, rule.max_speed);
+	}else if (state == State::kDevelop) { // Develop
+		hours_spent += 1;
+		lines_left_to_write -= current_speed;
+		current_speed = std::max(current_speed - rule.reduce_after_dev, 0);
 	}
 
-	if (hours_taken_ptr) {
-		*hours_taken_ptr = hours_taken;
+	if (lines_left_to_write <= 0) {
+		return hours_spent;
 	}
 
-	return pot;
+	return std::min(
+		get_hours(rule, hours_spent, current_speed, lines_left_to_write, State::kSleep),
+		get_hours(rule, hours_spent, current_speed, lines_left_to_write, State::kDevelop)
+	);
 }
 
 int main() {
 	Rule rule = get_input();
 	// Rule rule =	get_input_from_file("tc.txt");
-	Job job(0, rule.max_speed, rule.target);
 
-	while (job.get_lines_to_write_left() > 0) {
-		// std::cout << "BEF, Time: " << hours_spent << ", Left: " << lines_to_write_left << "/" << rule.target << ", Current speed: " << current_speed << std::endl;
-		if (job.get_current_speed() <= 0) { // speedが0になると寝るしかない
-			job.sleep(rule);
-		}
-
-		std::pair<int, int> current_potential_hours = Job::get_potential_and_hours(rule, job.get_current_speed());
-		int current_potential = current_potential_hours.first;
-		float current_efficiency = 1.0f * current_potential / current_potential_hours.second;
-
-		std::pair<int, int> sleep_potential_hours = Job::get_potential_and_hours(rule, std::min(job.get_current_speed() + rule.gain_after_sleep, rule.max_speed));
-		int potential_after_sleep = sleep_potential_hours.first;
-		float sleep_efficiency = 1.0f * potential_after_sleep / (sleep_potential_hours.second + 3);
-
-		// std::cout << "EFF: " << current_efficiency << " vs " << sleep_efficiency << std::endl;
-
-		// 寝るか開発かどっちが有利
-		if (current_potential < job.get_lines_to_write_left() && sleep_efficiency >= current_efficiency) {
-			// 寝る
-			job.sleep(rule);
-		} else {
-			// 開発
-			job.develop(rule);
-			// std::cout << "DEV" << std::endl;
-		}
-
-		// std::cout << "AFT, Time: " << hours_spent << ", Left: " << lines_to_write_left << "/" << rule.target << ", Current speed: " << current_speed << std::endl;
-		// std::cout << "--\n";
-	}
-
-	std::cout << job.get_hours_spent() << std::endl;
+	std::cout << get_hours(rule, 0, rule.max_speed, rule.target, State::kRoot) << std::endl;
 }
